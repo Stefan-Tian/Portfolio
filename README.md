9d57a142ee630bc48eff948eda899ee7de6d9522
Sheng Hong Tian <shenghongtian@MacBook-Pro-de-Sheng.local>
2017-11-29 11:52:04 +0800

    Created seeds file for sample data

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
-### Step one
-- rails new `project name` -T --database=postgresql(-T means no tests)
-- rails s (to start the server)
-- rails db:create 
-- rails db:migrate
-- rails g scaffold Blog title:string body:text
-- rails db:migrate
-- delete the scaffold.scss in app/assets/stylesheets
-- rails g controller Pages home about contact
-- rails g model Skill title:string percent_utilized:integer
-- rails g resource Portfolio title:string subtitle:string body:text `main_page`:text `thumb_image`:text
-_check rails -j= to add angular/react_
-_I can interact with database through rails -c_
-*rails new my_api --api (to not include frontend)*
-
-- git push origin controller-generator
-###MVC - Model - View - Controller
-
+### Portfolio App 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+  )
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "My skill #{skill}",
+    percent_utilized: 20
+  )
+end
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "My work #{portfolio_item}",
+    subtitle: "My great service",
+    body: "L'extrait standard de Lorem Ipsum utilisé depuis le XVIè siècle est reproduit ci-dessous pour les curieux. Les sections 1.10.32 et 1.10.33 du 'De Finibus Bonorum et Malorum' de Cicéron sont aussi reproduites dans leur version originale, accompagnée de la traduction anglaise de H. Rackham (1914).",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
